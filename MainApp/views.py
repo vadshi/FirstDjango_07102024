e8772498c29574fdd93deb01c85092e0df75e46f
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-10-10 20:40:36 +0300

    Done. M3:task1-2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
-
+from MainApp.models import Item
 
 # Create your views here.
 
 
-items = [
-   {"id": 1, "name": "Кроссовки abibas", "quantity":5},
-   {"id": 2, "name": "Куртка кожаная", "quantity":2},
-   {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
-   {"id": 7, "name": "Картофель фри", "quantity":0},
-   {"id": 8, "name": "Кепка", "quantity":124},
-]
+# items = [
+#    {"id": 1, "name": "Кроссовки abibas", "quantity":5},
+#    {"id": 2, "name": "Куртка кожаная", "quantity":2},
+#    {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
+#    {"id": 7, "name": "Картофель фри", "quantity":0},
+#    {"id": 8, "name": "Кепка", "quantity":124},
+# ]
 
 
 def home(request):
@@ -43,5 +43,6 @@ def get_item(request, item_id: int):
 
 
 def get_items(request):
+    items = Item.objects.all()
     context = {"items": items}
     return render(request, "items_list.html", context)

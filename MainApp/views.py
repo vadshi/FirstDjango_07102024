c0f50f6619c2612883ce6912d1cfbf781ea45722
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-10-10 21:36:18 +0300

    Remove comments

--- MainApp/views.py
+++ MainApp/views.py
@@ -4,16 +4,6 @@ from MainApp.models import Item
 
 # Create your views here.
 
-
-# items = [
-#    {"id": 1, "name": "Кроссовки abibas", "quantity":5},
-#    {"id": 2, "name": "Куртка кожаная", "quantity":2},
-#    {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
-#    {"id": 7, "name": "Картофель фри", "quantity":0},
-#    {"id": 8, "name": "Кепка", "quantity":124},
-# ]
-
-
 def home(request):
     context = {
         "name": "Иванов Петр Семенович",

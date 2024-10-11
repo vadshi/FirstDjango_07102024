923274eb83f81551a9aa3bbbdcee490b0617cfb5
Vadim <vadimx30@gmail.com>
2024-10-11 18:55:27 +0300

    Done. M3:part task1-3

--- MainApp/models.py
+++ MainApp/models.py
@@ -8,5 +8,6 @@ class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField() 
+    description = models.TextField(max_length=200, default="Базовое описание")
 
     objects: Manager  # <- this annotation
